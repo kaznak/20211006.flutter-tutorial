56bb04b0107e7b3fda530461c15ed28ca1f32508
kaznak <kaznak.at.work@gmail.com>
2021-10-06 23:06:15 +0900

    refactor: rename

--- lib/apps/my_app.dart
+++ lib/apps/my_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../pages/simple_counter.dart';
+import '../page/simple_counter.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
