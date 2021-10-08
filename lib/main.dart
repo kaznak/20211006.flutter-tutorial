7e27f9efbf5d6884932d6dba6ceae27ee223f9a6
kaznak <kaznak.at.work@gmail.com>
2021-10-08 13:23:22 +0900

    feat: lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'app/books_app.dart';
+import 'app/provider_counter_app.dart';
+// import 'app/books_app.dart';
 // import 'apps/my_app.dart';
 
 void main() {
-  runApp(const BooksApp());
+  runApp(const ProviderCounterApp());
+  // runApp(const BooksApp());
   // runApp(const MyApp());
 }
